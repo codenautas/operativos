5d19ee42722355a8be36af395cdfa32bf016d4d7
GracielaCanessa <gracane@gmail.com>
2018-05-28 16:55:42 -0300

    Borramos los que hacian referencia a bas-ope

--- server/server-bas-ope.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_operativos_1 = require("./app-operativos");
-const backend_plus_1 = require("backend-plus");
-var AppOperativos = app_operativos_1.emergeAppOperativos(backend_plus_1.AppBackend);
-new AppOperativos().start();
-//# sourceMappingURL=server-bas-ope.js.map
