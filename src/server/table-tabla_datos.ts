5b45d37e4d570e3906c19cd2d2fcf252c8e33114
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-08-16 12:13:57 -0300

    agrega columna para visualizar tablas codenautas/procesamiento#12

--- src/server/table-tabla_datos.ts
+++ src/server/table-tabla_datos.ts
@@ -15,7 +15,8 @@ function tabla_datos(context:TableContext):TableDefinition{
             {name:"tabla_datos"       , typeName:'text', nullable: false  ,},
             {name:"tipo"              , typeName:'text', nullable: false  ,},
             {name:"unidad_analisis"   , typeName:'text', nullable: false  ,},
-            {name:"generar"           , typeName: "bigint"  , editable:false, clientSide:'generarTD'}
+            {name:"generar"           , typeName: "bigint"  , editable:false, clientSide:'generarTD'},
+            {name:"ver_tabla"               , typeName: "bigint"  , editable:false, clientSide:'verTabla'}
         ],
         primaryKey:['operativo', 'tabla_datos'],
         foreignKeys:[
