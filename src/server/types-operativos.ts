6d8d0bd5c087c88c0e4e009a42535c0b9056d22c
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-11-01 16:39:43 -0300

    Se cambia de paradigma procedural a orientado a objetos (con algunos tweaks para que funcione en TS); Se agrega Clase OperativoGenerator encargada de encapsular la lógica en comun para generar tablas (calculadas o tablas de datos externas)

--- src/server/types-operativos.ts
+++ src/server/types-operativos.ts
@@ -1,5 +1,7 @@
-import {TableDefinition} from "backend-plus";
 import * as backendPlus from "backend-plus";
+import { TableDefinition } from "backend-plus";
+import { Client } from "pg-promise-strict";
+import { AppOperativos, AppOperativosType } from "./app-operativos";
 
 export * from "backend-plus";
 
@@ -28,14 +30,13 @@ export type VariableOpcion = {
     orden              :number
 }
 
-export interface Variable {
+export abstract class VariableDB {
     operativo          :string
     tabla_datos        :string
     variable           :string
     abr                :string
     nombre             :string
     tipovar            :string
-    unidad_analisis    :string
     clase              :string
     es_pk              :boolean
     es_nombre_unico    :boolean
@@ -50,51 +51,174 @@ export interface Variable {
     grupo              :string
     orden : number
 }
-export interface VariableComplete extends Variable {
+export class Variable extends VariableDB implements TipoVarDB {
     opciones: VariableOpcion[]
-    unidad_analisis: string
-};
-export interface UnidadDeAnalisis {
-    operativo : string
-    unidad_analisis : string
-    nombre : string
-    pk_agregada : string
-    padre : string
-    orden : number
-    principal : boolean
-}
-export interface TipoVar {
+
     tipovar: string
-    html_type: string
+    html_type?: string
+    type_name: backendPlus.PgKnownTypes
+    validar: string
+    radio: boolean
+
+    tdObj: TablaDatos
+
+    getFieldObject(){
+        if (this.tipovar == null) {
+            throw new Error('la variable ' + this.variable + ' no tiene tipo');
+        }
+        return { name: this.variable, typeName: this.type_name};
+    }
+
+    esCalculada(){
+        return this.clase == tiposTablaDato.calculada;
+    }
+
+    // async getTD(client: Client){
+    //     return await TablaDatos.fetchOne(client, this.operativo, this.tabla_datos);
+    // }
+
+    static hasTablePrefix(variable: string){
+        return variable.match(/^.+\..+$/);
+    }
+
+    static buildFromDBJSON(dbJson: Variable){
+        return Object.assign(new Variable, dbJson);
+    }
+
+    static async fetchAll(client:Client){
+        let query = `
+            SELECT v.*, tv.*, (SELECT jsonb_agg(to_jsonb(vo.*) order by vo.orden, vo.opcion) 
+                                FROM variables_opciones vo 
+                                WHERE vo.operativo = v.operativo and vo.tabla_datos = v.tabla_datos and vo.variable = v.variable) as opciones
+                FROM variables v LEFT JOIN tipovar tv USING(tipovar)
+                WHERE v.activa
+                ORDER BY es_pk desc, orden, variable`;
+        let resultV = await client.query(query).fetchAll();
+        return <Variable[]>resultV.rows.map((v:Variable) => Variable.buildFromDBJSON(v));
+    }
+}
+
+export interface TipoVarDB {
+    tipovar: string
+    html_type?: string
     type_name: backendPlus.PgKnownTypes
     validar: string
     radio: boolean
 }
 
-export interface TablaDatosDB {
+export abstract class TablaDatosDB {
     operativo: string
     tabla_datos: string
     tipo: tiposTablaDato
-    unidad_analisis: string
+    generada: Date
+    pks: string[]
+    que_busco: string
 }
 
-export class TablaDatos implements TablaDatosDB {
-    constructor(public operativo:string, public tabla_datos: string, public tipo: tiposTablaDato, public unidad_analisis: string){
+export class TablaDatos extends TablaDatosDB {
+
+    myVars: Variable[]
+
+    static buildFromDBJSON(dbJson: TablaDatosDB){
+        return Object.assign(new TablaDatos, dbJson);
+    }
+
+    static async fetchAll(client: Client):Promise<TablaDatos[]>{
+        let result = await client.query(`
+            SELECT td.*, r.que_busco, to_jsonb(array_agg(v.variable order by v.orden)) pks
+                FROM tabla_datos td 
+                    LEFT JOIN relaciones r ON td.operativo=r.operativo AND td.tabla_datos=r.tabla_datos AND r.tipo <> 'opcional' 
+                    LEFT JOIN variables v ON td.operativo=v.operativo AND td.tabla_datos=v.tabla_datos AND v.es_pk > 0
+                GROUP BY td.operativo, td.tabla_datos, r.que_busco`
+            , []).fetchAll();
+        return <TablaDatos[]> result.rows.map((td:TablaDatos) => TablaDatos.buildFromDBJSON(td));
     }
     
-    static construirConObj(tdDB: TablaDatosDB){
-        return new TablaDatos(tdDB.operativo, tdDB.tabla_datos, tdDB.tipo, tdDB.unidad_analisis);
+    static async fetchOne(client:Client, op: string, td:string){
+        //TODO sacar cosas comunes de la query afuera y parametrizar el where
+        let result = await client.query(`
+            SELECT td.*, r.que_busco, to_jsonb(array_agg(v.variable order by v.orden)) pks
+                FROM tabla_datos td 
+                    LEFT JOIN relaciones r ON td.operativo=r.operativo AND td.tabla_datos=r.tabla_datos AND r.tipo <>'opcional' 
+                    LEFT JOIN variables v ON td.operativo=v.operativo AND td.tabla_datos=v.tabla_datos AND v.es_pk > 0
+                WHERE td.operativo = $1 AND td.tabla_datos = $2
+                GROUP BY td.operativo, td.tabla_datos, r.que_busco`
+            , [op, td]).fetchOneRowIfExists();
+        return TablaDatos.buildFromDBJSON(<TablaDatos> result.row);
+    }
+    
+    // async getVars(client:Client){
+    //     return (await Variable.fetchAll(client)).filter(v=>v.tabla_datos==this.tabla_datos && v.operativo == this.operativo);
+    // }
+
+    getPKCSV(){
+        return this.pks.join(',');
+    }
+
+    getPrefixedQueBusco(){
+        return AppOperativos.prefixTableName(this.que_busco, this.operativo);
     }
 
     getTableName(){
-        return this.operativo.toLowerCase() + '_' + this.tabla_datos;
+        return AppOperativos.prefixTableName(this.tabla_datos, this.operativo);
     }
 
     esCalculada(){
         return this.tipo == tiposTablaDato.calculada
     }
 }
-export interface Operativo {
-    operativo: string
-    nombre: string
+
+export class OperativoGenerator{
+    // myTDs: {[key:string]: TablaDatos} = {}
+    // myVars: {[key:string]: Variable} = {}
+    myTDs: TablaDatos[]
+    myVars: Variable[]
+
+    static instanceObj: OperativoGenerator;
+
+    constructor(public app:AppOperativosType, public operativo?: string){
+        OperativoGenerator.instanceObj = this;
+    }
+
+    async fetchDataFromDB(client:Client){
+        this.myTDs = await TablaDatos.fetchAll(client);
+        this.myVars = await Variable.fetchAll(client);
+
+        if (this.operativo){
+            this.myTDs = this.myTDs.filter(td=>td.operativo==this.operativo);
+            this.myVars = this.myVars.filter(v=>v.operativo==this.operativo);
+        }
+
+        // OJO los TDs pueden repetirse por operativo y las vars se repiten por TD, entonces 
+        // si se hacen objetos indexados tener cuidado de no eliminar repetidos
+        // tds.forEach(td=> this.myTDs[td.tabla_datos]=td);
+        // vars.forEach(v=> this.myTDs[v.tabla_datos].myVars.push(v));
+    }
+
+    getVars(td:TablaDatos){
+        return this.myVars.filter(v => v.operativo == td.operativo && v.tabla_datos == td.tabla_datos);
+        // return likeAr(this.myVars).filter(v=>v.tabla_datos==td.tabla_datos).array();
+    }
+ 
+    getTD(v:Variable){
+        return this.myTDs.find(td => td.operativo == v.operativo && td.tabla_datos == v.tabla_datos);
+    }
+
+}
+
+export class Operativo {
+    constructor(public operativo: string, public nombre?: string){        
+    }
+
+    async getTDs(client:Client){
+        return (await TablaDatos.fetchAll(client)).filter(td=>td.operativo==this.operativo);
+    }
+
+    async getVars(client:Client){
+        return (await Variable.fetchAll(client)).filter(v=>v.operativo==this.operativo);
+    }
+
+    static async fetchAll(client:Client){
+        return <Operativo[]>(await client.query('SELECT * FROM operativos').fetchAll()).rows;
+    }
 }
