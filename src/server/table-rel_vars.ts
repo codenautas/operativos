beaa8927ae20a7781f5d47c5d83a6102040f5619
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2019-05-20 11:13:44 -0300

    adding fk to relaciones UK

--- src/server/table-rel_vars.ts
+++ src/server/table-rel_vars.ts
@@ -14,25 +14,21 @@ function rel_vars(context:TableContext):TableDefinition{
             { name: "operativo"          , typeName: 'text'    },
             { name: "tabla_datos"        , typeName: 'text'    },
             { name: "tiene"              , typeName: 'text'    },
-            { name: "orden"               , typeName: 'integer' },
-            // { name: "tabla_relacionada"  , typeName: 'text'    },
-            { name: "campo_datos"         , typeName: 'text'    },
-            { name: "campo_tiene"         , typeName: 'text'    },
-            { name: "dato_fijo"           , typeName: 'text'    },
-            { name: "funcion_dato"        , typeName: 'text' },
+            { name: "orden"              , typeName: 'integer' },
+            { name: "tabla_relacionada"  , typeName: 'text'    , nullable: false },
+            { name: "campo_datos"        , typeName: 'text'    },
+            { name: "campo_tiene"        , typeName: 'text'    },
+            { name: "dato_fijo"          , typeName: 'text'    },
+            { name: "funcion_dato"       , typeName: 'text'    },
         ],
         primaryKey: ['operativo', 'tabla_datos','tiene', 'orden'],
         foreignKeys: [
             {references:'operativos'   , fields:['operativo'] },
             {references:'tabla_datos'  , fields:['operativo','tabla_datos'] },
-            // {references:'relaciones'   , fields:['operativo', 'tabla_datos', 'tiene'], alias: 'rel_fk' },
-            // {references:'relaciones'   , fields:['operativo', 'tabla_datos', 'tiene', 'tabla_relacionada'], alias: 'fk_rel_uk' },
+            {references:'relaciones'   , fields:['operativo', 'tabla_datos', 'tiene'], alias: 'rel_fk' },
+            {references:'relaciones'   , fields:['operativo', 'tabla_datos', 'tiene', 'tabla_relacionada'], alias: 'fk_rel_uk' },
             {references:'variables'    , fields:['operativo', 'tabla_datos', {source:'campo_datos', target:'variable'}], alias:'campo_datos' },
             {references:'variables'    , fields:['operativo', {source:'tabla_relacionada', target:'tabla_datos'}, {source:'campo_tiene', target:'variable'}], alias:'campo_tiene' },
-        ],
-        // sql: {
-        //     fields:{},
-        //     postCreateSqls:'create trigger rel_var_tabla_relacionada_setting_trg before insert or update on rel_vars for each row execute procedure rel_var_tabla_relacionada_setting_trg();',
-        // }
+        ]
     }
 }
