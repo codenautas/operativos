218a1ea92e7e6136b0df46f772eb64eeaf40a0df
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2023-06-30 16:49:19 -0300

    typo in regexp

--- src/server/fun-no_numerica.ts
+++ src/server/fun-no_numerica.ts
@@ -3,10 +3,10 @@
 
 export const no_numerica = {dumpText:`--*/
 create or replace function no_numerica(p_texto text) returns boolean
-  language sql immutable 
+  language sql immutable
 as
 $sql$ 
-  select case when p_texto ~ '^-?(0|[1-9]\\d*)$' then null else true end;
+  select case when p_texto ~ '^-?(0|[1-9]\d*)$' then null else true end;
 $sql$;
 --`, testFixtures:`
 create or replace view casos_prueba_no_numerica as 
