6ab8a8f5a5872953653e55b24b1849ed4939f03a
manueldelapenna <manueldelapenna@gmail.com>
2026-09-02 10:32:28 -0300

    subo bp

--- src/server/table-parametros.ts
+++ src/server/table-parametros.ts
@@ -15,6 +15,7 @@ export function parametros(context: TableContext): TableDefinition {
         constraints:[
             {consName:'unico registro', constraintType:'check', expr:'unico_registro is true'}
         ],
+        //@ts-ignore no es necesario select
         sql:{
             postCreateSqls:'insert into parametros (unico_registro) values (true);'
         }
